42d3ecf9718be1caa5546c5798e37f16f3285e76
Alan Pelayo Zepeda <alanpelayozepeda@Alans-MacBook-Air.local>
2022-07-14 11:02:20 -0500

    Restructuring frontend

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
